ebd8c5b277af64cad140f86c46259829fb6edd8e
matthias <matthias.wagner.office@googlemail.com>
2012-09-14 19:39:32 -0700

    remote old libs

--- lib/detectors/lambda_libs/mashape/authentication/oauth10a_authentication.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), "/oauth_authentication.rb")
-
-module Mashape
-  class OAuth10aAuthentication < OAuthAuthentication
-     def initialize(consumer_key, consumer_secret, redirect_url)
-       super(consumer_key, consumer_secret, redirect_url)
-     end
-  end
-end
