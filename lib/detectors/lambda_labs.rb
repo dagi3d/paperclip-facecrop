16a2669864496c5f59e64b98d2194cb33226aece
matthias <matthias.wagner.office@googlemail.com>
2012-09-14 19:36:59 -0700

    fix

--- lib/detectors/lambda_labs.rb
+++ lib/detectors/lambda_labs.rb
@@ -4,10 +4,14 @@ class FaceCrop::Detector::LambdaLabs < FaceCrop::Detector::Base
   URL = "https://lambda-face-detection-and-recognition.p.mashape.com/detect"
 
   def detect_faces(file)
-
+    puts "boom"
+    puts @options
+    puts @options[:mashape_authorization]
     response = RestClient.post(URL, File.new(file),
                                {:content_type => 'json',
                                 "X-Mashape-Authorization" => @options[:mashape_authorization] })
+
+
 
     response = JSON.parse(response)
 
@@ -15,7 +19,7 @@ class FaceCrop::Detector::LambdaLabs < FaceCrop::Detector::Base
 
     photo = response['photos'].first
     photo['tags'].map do |tag|
-      # values are returned as percentual values
+      # values are returned as pixel values
 
       x = tag['center']['x'].to_i
       y = tag['center']['y'].to_i
